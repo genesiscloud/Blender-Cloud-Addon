af413059b067350e8f375e3da96a90958eff0a28
Sybren A. Stüvel <sybren@stuvel.eu>
2018-09-05 13:35:27 +0200

    Bumped version to 1.9.0

--- setup.py
+++ setup.py
@@ -228,7 +228,7 @@ setup(
               'wheels': BuildWheels},
     name='blender_cloud',
     description='The Blender Cloud addon allows browsing the Blender Cloud from Blender.',
-    version='1.9',
+    version='1.9.0',
     author='Sybren A. Stüvel',
     author_email='sybren@stuvel.eu',
     packages=find_packages('.'),
